04c3ad20f86a0e9a49f7d0ed9d991cd614d78c3d
Kukus89 <Kukus89@gmail.com>
2024-09-09 20:18:19 +0300

    home

--- cats/serializers.py
+++ cats/serializers.py
@@ -19,29 +19,21 @@ class CatSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
 
     def create(self, validated_data):
-        # Если в исходном запросе не было поля achievements
         if 'achievements' not in self.initial_data:
-            # То создаём запись о котике без его достижений
             cat = Cat.objects.create(**validated_data)
             return cat
 
-        # Иначе делаем следующее:
-        # Уберём список достижений из словаря validated_data и сохраним его
         achievements = validated_data.pop('achievements')
-        # Сначала добавляем котика в БД
         cat = Cat.objects.create(**validated_data)
-        # А потом добавляем его достижения в БД
         for achievement in achievements:
             current_achievement, status = Achievement.objects.get_or_create(
                 **achievement)
-            # И связываем каждое достижение с этим котиком
             AchievementCat.objects.create(
                 achievement=current_achievement, cat=cat)
         return cat
 
 
 class OwnerSerializer(serializers.ModelSerializer):
-    # cats = serializers.StringRelatedField(many=True, read_only=True)
     achievements = AchievementSerializer(many=True)
 
     class Meta:
